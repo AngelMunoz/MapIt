393287d7cbc0cd6a4a3b8a0e549c05f2b6f39dce
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-16 14:09:31 -0500

    add preliminary bits for PoC

--- MapIt/Runner.fs
+++ MapIt/Runner.fs
@@ -1,10 +1,64 @@
 module MapIt.Runner
 
+open System
+open System.IO
+open System.Text.Json
+open System.Text.Json.Serialization
+
 open FsToolkit.ErrorHandling
+open MapIt.Types
+
+
+let private (|ScopedPackage|Package|) (package: string) =
+    if package.StartsWith("@") then
+        ScopedPackage(package.Substring(1))
+    else
+        Package package
+
+let private parsePackageName (name: string) (withAt: bool) =
+
+    if name.Contains("@") then
+        let parts = name.Split("@")
+        let withAt = if withAt then "@" else ""
+
+        let version =
+            let version =
+                parts |> Seq.tryLast |> Option.defaultValue ""
+
+            if String.IsNullOrWhiteSpace version then
+                None
+            else
+                Some version
+
+        $"{withAt}{parts.[0]}", version
+    else
+        name, None
+
+
 
 let runInit options =
     result {
-        printfn "%A" options
+        let path =
+            $"{defaultArg options.path (Environment.CurrentDirectory)}/mapit.json"
+
+        let config =
+            { name = ""
+              importMapPath = "./public/imports.importmap" |> Some
+              dependencies = dict (Seq.empty<string * JsonElement>) |> Some }
+
+        let bytes =
+            let opts =
+                let opts = JsonSerializerOptions()
+                opts.WriteIndented <- true
+                opts
+
+            JsonSerializer.SerializeToUtf8Bytes(config, options = opts)
+
+        try
+            File.WriteAllBytes(path, bytes)
+        with
+        | ex -> return! ex |> Error
+
         return 0
     }
 
@@ -22,6 +76,25 @@ let runShow options =
 
 let runInstall options =
     result {
-        printfn "%A" options
+        let! package, version =
+            match options.package with
+            | Some (ScopedPackage package) -> parsePackageName package true |> Ok
+            | Some (Package package) -> parsePackageName package false |> Ok
+            | None -> MissingPackageNameException |> Error
+
+        let alias =
+            options.alias |> Option.defaultValue package
+
+        let map =
+            let version =
+                match version with
+                | Some version -> $"@{version}"
+                | None -> ""
+
+            { imports = dict [ alias, $"https://cdn.skypack.dev/{package}{version}" ]
+              scopes = dict (Seq.empty) }
+            |> JsonSerializer.Serialize
+
+        printfn "%A" map
         return 0
     }
